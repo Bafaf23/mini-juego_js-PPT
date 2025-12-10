93a23b7475ac6f00c99de5e1d710edd449eeacf5
Bryant Facenda <bryantffacen@gmail.com>
2025-12-09 22:49:50 -0400

    Esqueleto html y creacion de archivos

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/**
+ * Crear un juego de piedra, papel o tijeras con interfaz gráfica usando
+ * HTML, CSS (TailwindCSS, SASS, etc.) y JavaScript.
+ *
+ * La partida debe permitir al usuario seleccionar una de las tres opciones
+ * (piedra, papel o tijeras) mediante botones o imágenes.
+ *
+ * La computadora debe seleccionar aleatoriamente una de las tres opciones.
+ *
+ * La interfaz debe mostrar el resultado de cada ronda (ganador, perdedor o empate)
+ * y llevar un registro de las siguientes estadísticas:
+ * - Número total de rondas jugadas.
+ * - Número de victorias del usuario.
+ * - Número de victorias de la computadora.
+ * - Número de empates.
+ * - Registro de las últimas 5 rondas jugadas (opciones seleccionadas por el usuario y la computadora).
+ *
+ * Las estadísticas deben actualizarse dinámicamente en la interfaz después de cada ronda y ser
+ * accesibles para el usuario en todo momento, incluso cuaando este recarga la página
+ * (o cierra y vuelve a abrir el navegador).
+ *
+ * Incluir un botón para reiniciar las estadísticas y comenzar una nueva partida.
+ *
+ * El código debe estar documentado utilizando JSDoc.
+ *
+ * Opcional: Agregar modo claro/oscuro para la interfaz del juego con persistencia de la preferencia
+ * del usuario utilizando almacenamiento local (localStorage).
+ *
+ * 2025/12/12
+ */
